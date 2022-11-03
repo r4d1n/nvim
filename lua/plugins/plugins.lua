223a71355aef16413df6b60829f81f040f94fa8b
Jared Radin <jared@r4d1n.net>
2022-11-03 08:52:21 -0700

    remove plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -2,7 +2,6 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-  use 'terroo/vim-simple-emoji'
   use 'navarasu/onedark.nvim'
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/cmp-nvim-lsp'
