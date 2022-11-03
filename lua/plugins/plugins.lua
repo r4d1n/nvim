9de740246f2aecbbabb45d806c17c1fd75b26a12
Jared Radin <jared@r4d1n.net>
2022-11-03 11:36:29 -0700

    add some plugins, change indentation

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,17 +1,20 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'navarasu/onedark.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'tomtom/tcomment_vim'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/nvim-cmp'
-  use 'editorconfig/editorconfig-vim'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use 'wbthomason/packer.nvim'
+    use 'navarasu/onedark.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'tomtom/tcomment_vim'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/nvim-cmp'
+    use 'editorconfig/editorconfig-vim'
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'airblade/vim-gitgutter'
+    use 'airblade/vim-rooter'
+    use 'Townk/vim-autoclose'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 end)
