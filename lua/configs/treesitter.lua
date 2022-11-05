c9fc1b3b45d1d9aa2505d44f1d73265df95a910b
Jared Radin <jared@r4d1n.net>
2022-11-05 12:01:59 -0700

    add treesitter folding and incremental selection

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -2,14 +2,28 @@ require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all"
 	ensure_installed = { "javascript", "typescript", "tsx", "json", "lua" },
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
-
 	-- Automatically install missing parsers when entering buffer
 	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
+	-- auto_install = true,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	-- sync_install = false,
 
 	highlight = {
 		enable = true,
 	},
+
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<CR>",
+			scope_incremental = "<CR>",
+			node_incremental = "<TAB>",
+			node_decremental = "<S-TAB>",
+		},
+	},
 })
+
+vim.o.foldmethod = "expr"
+vim.o.foldcolumn = "auto"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldenable = false -- Disable folding at startup.
