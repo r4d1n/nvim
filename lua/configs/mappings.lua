474d126b4113a65164f4fd50182d6edbfcc66a29
Jared Radin <jared@r4d1n.net>
2022-11-08 11:20:13 -0800

    tweak telescope mappings again

--- lua/configs/mappings.lua
+++ lua/configs/mappings.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
 -- telescope
-vim.cmd([[nnoremap <Leader>f :lua require'telescope.builtin'.find_files{} <CR>]])
-vim.cmd([[nnoremap <Leader>\ :lua require'telescope.builtin'.live_grep{} <CR>]])
-vim.cmd([[nnoremap <Leader>g :lua require'telescope.builtin'.grep_string{} <CR>]])
+vim.cmd([[nnoremap <Leader>ff :lua require'telescope.builtin'.find_files{} <CR>]])
+vim.cmd([[nnoremap <Leader>rg :lua require'telescope.builtin'.live_grep{} <CR>]])
+vim.cmd([[nnoremap <Leader>gs :lua require'telescope.builtin'.grep_string{} <CR>]])
