48916512ce4c133d6cf483b56994529da0f3abe4
Jared Radin <jared@r4d1n.net>
2023-10-20 13:16:03 -0700

    migrate package manager and reorganize configs
    migrate formatting
    
    reorganize and use more new plugins
    
    reorganize and use more new plugins
    
    change keymapping style
    
    more mini modules
    
    add which-key
    
    swap git plugin
    
    another mini module
    
    move formatting keymap
    
    reorganize telescope
    
    add typescript-tools plugin

---
+++ lua/plugins/init.lua
@@ -0,0 +1,41 @@
+return {
+	"neovim/nvim-lspconfig",
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+	"nvim-lua/plenary.nvim",
+	{ "echasnovski/mini.nvim", version = "*" },
+	"nvim-tree/nvim-web-devicons",
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	"stevearc/conform.nvim",
+	"lewis6991/gitsigns.nvim",
+	"airblade/vim-rooter",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	{ "sindrets/diffview.nvim", dependencies = "nvim-lua/plenary.nvim" },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "elixir-tools/elixir-tools.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+}
