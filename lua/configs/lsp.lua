fa98a064450cb5a06ef0d3159b731b05441b3453
Jared Radin <jared@r4d1n.net>
2023-10-21 13:23:13 -0700

    finish cmp setup

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,38 +1,13 @@
-local cmp = require("cmp")
-cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			else
-				fallback()
-			end
-		end,
-		["<S-Tab>"] = function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end,
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-	}, {
-		{ name = "buffer" },
-	}),
-})
-
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
--- require("lspconfig").tsserver.setup({ capabilities = capabilities }) -- commented in favor of the typescript tools plugin
+
+-- require("lspconfig").tsserver.setup({ capabilities = cmp.capabilities }) -- commented in favor of the typescript tools plugin
 require("lspconfig").eslint.setup({ capabilities = capabilities })
 require("lspconfig").graphql.setup({ capabilities = capabilities })
 require("lspconfig").lua_ls.setup({ capabilities = capabilities })
 require("lspconfig").prismals.setup({ capabilities = capabilities })
 require("lspconfig").tailwindcss.setup({ capabilities = capabilities })
 require("lspconfig").html.setup({ filetypes = { "html", "handlebars" } })
+
 require("elixir").setup()
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
