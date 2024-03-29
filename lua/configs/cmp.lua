b33557694ab6a13584066244a8e95abbfdfc9203
Jared Radin <jared@r4d1n.net>
2024-03-29 13:04:22 -0700

    fix config for elixir

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -30,9 +30,7 @@ cmp.setup({
 		{ name = "nvim_lsp" },
 		{ name = "nvim_lsp_signature_help" },
 		{ name = "luasnip" },
-		{
-			{ name = "buffer" },
-		},
+		{ name = "buffer" },
 	}),
 })
 
--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -9,7 +9,20 @@ require("lspconfig").tailwindcss.setup({ capabilities = capabilities })
 require("lspconfig").html.setup({ filetypes = { "html", "handlebars" } })
 require("lspconfig").rust_analyzer.setup({})
 
-require("elixir").setup()
+require("elixir").setup({
+	nextls = {
+		enable = true,
+		init_options = {
+			experimental = {
+				completions = {
+					enable = true, -- control if completions are enabled. defaults to false
+				},
+			},
+		},
+	},
+	credo = { enable = true },
+	elixirls = { enable = false },
+})
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	underline = true,
